f35ffc3108bc62a8f63ae4d1ed92d2213f19665f
breno santos <breno.angelim@hotmail.com>
2025-05-25 22:43:39 -0300

    fix: fix response json from file controller

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 import configs
 import routes
 
 def create_app():
     app = Flask(__name__)
-    
+    CORS(app)
     configs.init_app(app)
     routes.init_app(app)
 
--- controllers/files_controller.py
+++ controllers/files_controller.py
@@ -5,31 +5,31 @@ from services.file_service import save_file, get_user_files
 
 def upload_files():
     if "file" not in request.files:
-        return jsonify({"error": "Nenhum arquivo enviado"}), 400
+        return jsonify({"message": "Nenhum arquivo enviado"}), 400
 
     file = request.files["file"]
     data = request.form.to_dict(flat=False)
     print("file.mimetype", file.mimetype)
     print("file.filename", file.filename)
     if not data.get("user_id"):
-        return jsonify({"error": "Usuário não especificado"}), 400
+        return jsonify({"message": "Usuário não especificado"}), 400
 
     try:
         # Salva o arquivo e registra no banco
         file_record = save_file(data["user_id"], file)
         return jsonify(file_record), 201
     except Exception as e:
-        return jsonify({"error": f"Erro ao salvar o arquivo: {str(e)}"}), 400
+        return jsonify({"message": f"Erro ao salvar o arquivo: {str(e)}"}), 400
 
 
 def get_files_db_by_user(user_id):
     if not user_id:
-        return jsonify({"error": "Usuário não especificado"}), 400
+        return jsonify({"message": "Usuário não especificado"}), 400
     try:
         files = get_user_files(user_id)
         return jsonify(files), 200
     except Exception as e:
-        return jsonify({"error": f"Erro ao buscar arquivos: {str(e)}"}), 500
+        return jsonify({"message": f"Erro ao buscar arquivos: {str(e)}"}), 500
 
 
 def get_files_by_zip(zip_id):
